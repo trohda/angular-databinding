3259026fe9b84dd4de8e6792724635ce5c2b2726
Tomasz Rohda <tomaszrohda@wp.pl>
2022-09-02 10:16:10 +0200

    Change two-way binding to local references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -11,18 +11,23 @@ export class CockpitComponent implements OnInit {
   @Output("bpCreated")
   blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer() {
-this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement) {
+
+this.serverCreated.emit({
+  serverName: nameInput.value,
+  serverContent: this.newServerContent});
   }
 
-  onAddBlueprint() {
-  this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBlueprint(nameInput: HTMLInputElement) {
+  this.blueprintCreated.emit({
+    serverName: nameInput.value,
+    serverContent: this.newServerContent});
 
   }
 }
